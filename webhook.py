b23cee58a8f462632b84328ecdfedf26394c6ce4
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-12-04 09:21:22 +0200

    Update webhook.py

--- webhook.py
+++ webhook.py
@@ -1,11 +1,15 @@
-from discord_webhook import DiscordWebhook, DiscordEmbed
-from tools import get_json_data, get_json_decode
+import discord
+from discord import Webhook, RequestsWebhookAdapter
+from utils import get_json_data, random_color
+from utils import get_json_decode
+from datetime import datetime
 
 DATA = get_json_data()
 
 WEBHOOK_URL = DATA["webhook_url"]
+
 if not(WEBHOOK_URL):
-	raise Exception("Webhook url is empty")
+    raise ValueError("Webhook url is empty")
 
 # Test Data
 """
@@ -28,20 +32,21 @@ def webhook_send(json_):
 	if not(json_['is_public']):
 		return
 
-	webhook = DiscordWebhook(url = WEBHOOK_URL)
+	webhook = Webhook.from_url(WEBHOOK_URL, adapter = RequestsWebhookAdapter())
 
 	# Embed
-	embed = DiscordEmbed(title = 'DONATE!', color = 0x4d1a9a)
+	embed = discord.Embed(title = "Donate!",
+                          timestamp = datetime.utcnow(),
+                          color = random_color())
 
 	embed.add_embed_field(value = f"{json_['from_name'].replace('+', ' ')}",   name = "Donater", inline = False)
 	embed.add_embed_field(value = f"{json_['message'].replace('+', ' ')}",     name = "Message", inline = False)
 	embed.add_embed_field(value = f"{json_['amount']}$",                       name = "Amount of money", inline = False)
-	embed.add_embed_field(value = f"{json_['email']}",                         name = "Email", inline = False)
-	embed.add_embed_field(value = f"{json_['kofi_transaction_id']}",           name = "Transaction id", inline = False)
-	embed.add_embed_field(value = f"{json_['is_subscription_payment']}",       name = "Subscription payment", inline = False)
-	embed.add_embed_field(value = f"{json_['is_first_subscription_payment']}", name = "First subscription payment", inline = False)
+	#embed.add_embed_field(value = f"{json_['email']}",                         name = "Email", inline = False)
+	#embed.add_embed_field(value = f"{json_['kofi_transaction_id']}",           name = "Transaction id", inline = False)
+	#embed.add_embed_field(value = f"{json_['is_subscription_payment']}",       name = "Subscription payment", inline = False)
+	#embed.add_embed_field(value = f"{json_['is_first_subscription_payment']}", name = "First subscription payment", inline = False)
 
-	embed.set_footer(text = f"{json_['timestamp'][:10]} • {json_['timestamp'][11:19]}")
+	#embed.set_footer(text = f"{json_['timestamp'][:10]} • {json_['timestamp'][11:19]}")
 
-	webhook.add_embed(embed)
-	webhook.execute()
+	webhook.send(embed = embed)
