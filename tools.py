741ac9f3db19cdcf155c758d51dd63af926d5b18
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-12-04 09:17:02 +0200

    Update tools.py

--- tools.py
+++ tools.py
@@ -1,13 +1,17 @@
 import json
+import discord
 from urllib.parse import unquote # Url Decoder
+from random import randint
 
+def random_color():
+    return discord.Color.from_rgb(randint(1, 255), randint(1, 255), randint(1, 255))
 
-def get_json_data(config = "config.json"):
+def get_json_data(config = "config.json") -> None:
 	# Open and json reading
 	with open(config, "r") as file:
 		return json.load(file)
 
-def get_json_decode(data):
+def get_json_decode(data) -> str:
 	dirty_json = unquote(str(data))
 	json_ = json.loads(dirty_json[7:][:-1]) # Trim the excess for Json
 	return json_
