2b9b7e527d2f2282fb3489b5214d3c072a641fab
LEv145 <40071054+LEv145@users.noreply.github.com>
2020-12-05 10:50:16 +0200

    Rewrite on aiohttp:3

--- server.py
+++ server.py
@@ -1,32 +1,40 @@
-from flask import Flask, request
+from aiohttp import web
 from tools import get_json_data, get_json_decode
 from webhook import webhook_send
-
 
 DATA = get_json_data()
 
 KOFI_IP = DATA["kofi_ip"]
 PORT    = DATA["port"]
 if not(PORT): # Is empty
-	raise ValueError("Port is empty")	
+	raise Exception("Port is empty")	
 
-app = Flask(__name__)
+routes = web.RouteTableDef() # routes
 
-@app.route("/gets", methods = ["POST"])
-def receive_update():
-	ip = request.remote_addr		
+@routes.post('/gets')
+async def receive_update(request):
+	ip = request.remote	
+	print(ip)	
+	
+	data = await request.read()
 
 	if KOFI_IP != "":
 		if ip == KOFI_IP: # Checking for ip
-			json_ = get_json_decode(request.get_data())
+			json_ = get_json_decode(data)
 			webhook_send(json_)
 	else: # If the parameter is empty
 		try:
-			json_ = get_json_decode(request.get_data())
+			json_ = get_json_decode(data)
 			webhook_send(json_)
 		except Exception as e:
 			print(e)
-	return "OK"
+	return web.Response(text='OK')
 
-if __name__ == '__main__':
-	app.run(host = '0.0.0.0', port = PORT)
+# Init Server
+async def init_app() -> web.Application:
+    app = web.Application()
+    app.add_routes(routes)
+    return app
+
+
+web.run_app(init_app(), host="0.0.0.0", port=PORT)
